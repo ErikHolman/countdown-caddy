c6d245ed7842b5b9704a3931f3d82749d05c7c6c
ErikHolman <lifeinthedayof@gmail.com>
2023-09-06 17:41:02 -0700

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,10 @@ import './footer.scss';
 function Footer() {
   return (
     <div className='footer'>
-      <div className='text'>BEHOLD! a footer.</div>
+      <div className='text'>
+        BEHOLD! a footer.{' '}
+        <div className='secret'>ooooohhhhh a secret message</div>
+      </div>
     </div>
   );
 }
